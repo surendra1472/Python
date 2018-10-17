621d228554543bcc4e18b52c20dfce2a243dfca1
surendra1472 <g.surendra1472@gmail.com>
2018-10-17 08:39:04 +0530

    first commit

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,15 @@
+
+class HelloWorld:
+
+    def __init__(self, name):
+        self.sayHello(name)
+
+    def sayHello(self, name):
+        print("Hello {0}".format(name))
+
+
+
+
+if(__name__ == "__main__"):
+    helloWorld = HelloWorld("World")
+    helloWorld = HelloWorld("Lucy")
